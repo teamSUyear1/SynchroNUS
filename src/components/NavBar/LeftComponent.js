81396ca6d1af74aea3d68f13d1d26d1692c198a1
Huang Hao <huang_hao12@hotmail.com>
2022-05-27 13:52:10 +0800

    Install dependences

---
+++ src/components/NavBar/LeftComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LeftComponent() {
+  return (
+    <div>LeftComponent</div>
+  )
+}
+
+export default LeftComponent
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from "../config/firebaseConfig";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import React, { useState, useEffect, useContext, createContext } from "react";
