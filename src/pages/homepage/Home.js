f551fdb88fef5cf5f6a5b2174cc46d7ded837356
“douglch” <douglchwm@gmail.com>
2022-06-01 11:19:33 +0800

    Added video background to homepage with css

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,16 +1,26 @@
 import { Paper, Typography } from "@mui/material";
 import vidBackground from "../../components/Assets/test.mp4";
 import React from "react";
+import classes from "./Home.module.css";
 
 function Home() {
   return (
     /*React.Fragment is just an empty container to enable 
     a single element return for the function*/
     <React.Fragment>
-      <video src={vidBackground} autoPlay loop muted />
-      <Typography variant="h6" color="inherit">
+      <div className={classes.main}>
+        <div className={classes.overlay} />
+        <video src={vidBackground} autoPlay loop muted />
+        {/* <Typography variant="h6" color="inherit">
         This is Home page
-      </Typography>
+      </Typography> 
+      Will be removing this because Typography shifts all texts to the bottom of the video,
+      not sure how to fix
+      */}
+        <div className={classes.content}>
+          This is the homepage
+        </div>
+      </div>
     </React.Fragment>
   );
 }
