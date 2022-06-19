087968e277ac66c7cf558913d77b75a7705289a9
douglch <douglchwm@gmail.com>
2022-06-19 16:15:33 +0800

    minor UI improvements

--- src/pages/timerpage/Settings.js
+++ src/pages/timerpage/Settings.js
@@ -3,45 +3,56 @@
   allows users to config break and session time
 */
 import { Grid, Typography } from "@mui/material";
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import { Button } from "@mui/material";
 import TimeConfigUI from "./TimeConfigUI";
 import SettingsContext from "./SettingsContext";
+import classes from "./Settings.module.css";
 
 const Settings = (props) => {
   const settingsInfo = useContext(SettingsContext);
 
   const sessionStartHandler = () => {
-    return (settingsInfo.setShowSettings(false))
-  }
+    return settingsInfo.setShowSettings(false);
+  };
 
   return (
-    <Grid container justifyContent={"center"}>
-      <Grid item height={"80vh"}>
-        <Typography variant="h2" color="inherit">
-          Pick a Time
-        </Typography>
-        <Grid container justifyContent={"center"}>
-          <TimeConfigUI
-            title={"Break length"}
-            changeTime={settingsInfo.changeTimeHandler}
-            type={"break"}
-            time={settingsInfo.breakTime}
-            formatTime={settingsInfo.formatTimeHandler}
-          />
-          <TimeConfigUI
-            title={"Session length"}
-            changeTime={settingsInfo.changeTimeHandler}
-            type={"session"}
-            time={settingsInfo.sessionTime}
-            formatTime={settingsInfo.formatTimeHandler}
-          />
+    <Fragment>
+      <Grid container justifyContent={"center"}>
+        <Grid item>
+          <Typography variant="h2" color="inherit">
+            Pick a Time
+          </Typography>
         </Grid>
-        <Button title="Start timer" variant="outlined" color="success" onClick={sessionStartHandler}>
+      </Grid>
+      <Grid container direction={"row"} justifyContent={"center"} sx={{gap: "5px"}}>
+        <TimeConfigUI
+          title={"Break length"}
+          changeTime={settingsInfo.changeTimeHandler}
+          type={"break"}
+          time={settingsInfo.breakTime}
+          formatTime={settingsInfo.formatTimeHandler}
+        />
+        <TimeConfigUI
+          title={"Session length"}
+          changeTime={settingsInfo.changeTimeHandler}
+          type={"session"}
+          time={settingsInfo.sessionTime}
+          formatTime={settingsInfo.formatTimeHandler}
+        />
+      </Grid>
+      <Grid container justifyContent={"center"}>
+        <Button
+          title="Start timer"
+          variant="outlined"
+          color="success"
+          onClick={sessionStartHandler}
+        >
           Start!
         </Button>
       </Grid>
-    </Grid>
+      <Grid item height={"50vh"} />
+    </Fragment>
   );
 };
 
--- src/pages/timerpage/TimeConfigUI.js
+++ src/pages/timerpage/TimeConfigUI.js
@@ -1,20 +1,28 @@
-import React from "react";
+import { Fragment } from "react";
 import { IconButton } from "@mui/material";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import IndeterminateCheckBoxOutlinedIcon from "@mui/icons-material/IndeterminateCheckBoxOutlined";
 
 const TimeConfigUI = (props) => {
   return (
-    <React.Fragment>
-      <h3 style={{color: "white"}}>{props.title}</h3>
-      <IconButton title="Minus 30 seconds" onClick={() => props.changeTime(-30, props.type)}>
+    <Fragment>
+      <h3 style={{ color: "white" , marginTop:"10px"}}>{props.title}</h3>
+      <IconButton
+        title="Minus 30 seconds"
+        onClick={() => props.changeTime(-30, props.type)}
+      >
         <IndeterminateCheckBoxOutlinedIcon color="primary" />
       </IconButton>
-      <h3 style={{color: "white"}}>{props.formatTime(props.time)}</h3>
-      <IconButton title="Add 30 seconds" onClick={() => props.changeTime(30, props.type)}>
+      <h3 style={{ color: "white", marginTop:"11px"}}>
+        {props.formatTime(props.time)}
+      </h3>
+      <IconButton
+        title="Add 30 seconds"
+        onClick={() => props.changeTime(30, props.type)}
+      >
         <AddBoxOutlinedIcon color="primary" />
       </IconButton>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
