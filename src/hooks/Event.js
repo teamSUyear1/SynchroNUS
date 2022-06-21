ab5589b281011cc242c0491dd6904378f39b5391
Huang Hao <huang_hao12@hotmail.com>
2022-06-21 23:50:05 +0800

    Add event hooks

---
+++ src/hooks/Event.js
@@ -0,0 +1,26 @@
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useAuth, db } from './useAuth'
+
+function Event() {
+    const [events, setEventsState] = useState([])
+    const {user} = useAuth();
+    const assignmentRef = doc(db, "assignments", user?.uid)
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(assignmentRef, snapshot => {
+            setEventsState(snapshot.data().tasks)
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [user.uid])
+
+  return {
+    events,
+    setEventsState
+  }
+}
+
+export default Event
