8d675e7a45e3eaf6ecb918c4567a16213cadab4c
douglch <douglchwm@gmail.com>
2022-06-23 16:24:25 +0800

    Time configuration boxes changed

--- src/pages/timerpage/SettingsCardUI.js
+++ src/pages/timerpage/SettingsCardUI.js
@@ -1,4 +1,3 @@
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
--- src/pages/timerpage/TimeConfigUI.js
+++ src/pages/timerpage/TimeConfigUI.js
@@ -1,37 +1,63 @@
 import { Fragment } from "react";
-import { IconButton } from "@mui/material";
+import { IconButton, Typography, Button, Grid } from "@mui/material";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import IndeterminateCheckBoxOutlinedIcon from "@mui/icons-material/IndeterminateCheckBoxOutlined";
 import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 
 const TimeConfigUI = (props) => {
-  const onChangeHandler = (event) => {
-    const colon = ":";
+  const onTimeChangeHandler = (event) => {
+    props.timeSet(event.target.valueAsNumber);
   };
 
   return (
     <Fragment>
-      <IconButton
-        title="Minus 30 seconds"
-        onClick={() => props.changeTime(-30, props.type)}
-      >
-        <IndeterminateCheckBoxOutlinedIcon color="primary" />
-      </IconButton>
-      <h3 style={{ color: "white", marginTop: "11px" }}>
-        {props.formatTime(props.time)}
-      </h3>
-      <TextField
-        id="outlined-number"
-        label={props.title}
-        defaultValue={props.formatTime(props.time)}
-        //value to be changed
-      />
-      <IconButton
-        title="Add 30 seconds"
-        onClick={() => props.changeTime(30, props.type)}
-      >
-        <AddBoxOutlinedIcon color="primary" />
-      </IconButton>
+      <Card variant="outlined" sx={{ minWidth: 200, mr: "20px" }}>
+        {/* 
+          <CardContent sx={{ display: "flex", direction: "column" }}>
+          UI for click to add time buttons
+
+          <IconButton
+            title="Minus 30 seconds"
+            onClick={() => props.changeTime(-30, props.type)}
+          >
+            <IndeterminateCheckBoxOutlinedIcon color="primary" />
+          </IconButton>
+          <h3 style={{ color: "white", marginTop: "11px" }}>
+            {props.formatTime(props.time)}
+          </h3>
+          <IconButton
+            title="Add 30 seconds"
+            onClick={() => props.changeTime(30, props.type)}
+          >
+            <AddBoxOutlinedIcon color="primary" />
+          </IconButton> 
+        </CardContent> */}
+        <CardContent>
+          <Typography sx={{ fontWeight: "bold" }}>
+            Set a {props.type} length
+          </Typography>
+          {props.type === "break" && <Typography sx={{fontStyle: "italic", mb: "10px"}} fontSize="15px">Leave it empty for no breaks</Typography>}
+          {props.type === "session" && <Grid item height={"33px"} />}
+          <TextField
+            color="secondary"
+            type="number"
+            placeholder="up to 3 digits"
+            label={props.title + " (in minutes)"}
+            onInput={(e) => {
+              e.target.value = Math.max(0, parseInt(e.target.value))
+                .toString()
+                .slice(0, 3);
+            }}
+            onChange={onTimeChangeHandler}
+          />
+          {/* <Button variant="outlined" sx={{ mt: "1vh", ml: "1vh" }} onClick={}>
+            Set
+          </Button> */}
+        </CardContent>
+      </Card>
     </Fragment>
   );
 };
